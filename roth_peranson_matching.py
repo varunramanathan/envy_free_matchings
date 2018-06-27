817c14ab59ec4ade3d890a6c3ce1827f20bb1052
Varun Ramanathan <varun28031999@gmail.com>
2018-06-27 16:42:08 +0530

    Working for the input.txt, added helpful comments, have to check for more test cases

--- roth_peranson_matching.py
+++ roth_peranson_matching.py
@@ -3,15 +3,16 @@ import heapq
 input starts with number of hospitals and number of doctors space separated
 For each hospital, there are two lines of input
 the first line contains "l u" (without the quotes) which are the lower and upper quotas
-the second line contains a list of space seperated values, the ith of which denotes the preference given to the
-ith doctor. if the ith value is -1, then the doctor is unacceptable
+the second line contains a list of space seperated values, the ith of which denotes the ith preferred doctor
 
 For each doctor, there is a single line of input
-The line contains a list of space seperated values, the ith of which denotes the preference given to the
-ith hospital. if the ith value is -1, then the hospital is unacceptable
+The line contains a list of space seperated values, the ith of which denotes the ith preferred hospital
 '''
 
-h,d = list(map(int,input().rstrip().split(" ")))
+hd = list(map(int,input().rstrip().split(" ")))
+h = hd[0]
+d = hd[1]
+print(h)
 h_l_u = [] # (lower quota,upper quota) of hospital i at ith index
 h_list = dict() # a dict of (key,value)->(doctor,preference) of hospital i at ith index
 # # h_list_dict = dict()
@@ -23,44 +24,77 @@ for i in range(h):
     lu = list(map(int,input().rstrip().split(" ")))
     h_l_u.append((lu[0],lu[1]))
     ar = list(map(int,input().rstrip().split(" ")))
-    h_list[i] = ({ar[j]:j for j in range(len(ar)) if ar[j]!=-1},{j:ar[j] for j in range(len(ar)) if ar[j]!=-1})
+    h_list[i] = ({ar[j]:j for j in range(len(ar))},{j:ar[j] for j in range(len(ar))})
+print(h_list)
 for i in range(d):
     ar = list(map(int, input().rstrip().split(" ")))
-    d_list[i] = ({ar[j]:j for j in range(len(ar)) if ar[j]!=-1},{j:ar[j] for j in range(len(ar)) if ar[j]!=-1})
-
+    d_list[i] = ({ar[j]:j for j in range(len(ar))},{j:ar[j] for j in range(len(ar))})
+print(d_list)
 unassigned = set([i for i in range(d)])
+print("unassigned = "+str(unassigned))
 assigned = set()
 cant_assign = set()
 start_from = [0 for i in range(d)]
 h_docs_assigned_heaps = [[] for i in range(h)]
 while(unassigned):
     d = unassigned.pop()
-    for h in d_list[d][2]:
-        if h<start_from[d]: continue
+    unassigned.add(d)
+    print("popping "+str(d)+" right now (but also added it)")
+    for h in d_list[d][0]:
+        print("looking at hospital "+str(h))
+        if h<start_from[d]:
+            print("already looked at this hospital for this doctor")
+            continue #if you have already evaluated this hospital, you won't need to come here again
         start_from[d] = h+1
-        if d not in h_list[h][0]:
+        if d not in h_list[h][0]: #d is not acceptable for hospital h
+            print(str(d)+" is not accepted by "+str(h))
             continue
         else:
             if len(h_docs_assigned_heaps[h])==h_l_u[h][0]: #all places are filled
-                if h_docs_assigned_heaps[h][0]>h_list[h][0][d]: #if d can replace someone
-                    #add d to assigned and add d to h_docs_assigned_heaps[h][0]
-                    assigned.add(d)
-                    unassigned.remove(d)
-                    heapq.heappush(h_docs_assigned_heaps[h],h_list[h][0][d])
-                     #remove the lowest preferred doc who had been assigned earlier
-                    min_doc_removed_pref = heapq.heappop(h_docs_assigned_heaps[h])
-                    min_doc_removed = h_list[h][1][min_doc_removed_pref]
-                    assigned.remove(min_doc_removed)
-                    unassigned.add(min_doc_removed)
-                else:
-                    continue
-            else:
-                heapq.heappush(h_docs_assigned_heaps[h],h_list[h][0][d])
-                assigned.add(d)
-                unassigned.remove(d)
-    if start_from[d]>h:
-        if d in unassigned: unassigned.remove(d)
-        else: cant_assign.add(d)
+                print("all the places have been filled at "+str(h))
+                min_doc_removed_pref = heapq.heappop(h_docs_assigned_heaps[h]) #the worst ranked doc's ranking
+                min_doc_removed = h_list[h][1][min_doc_removed_pref]
 
-for i in range(h):
-    print("Doctors in "+h+" are "+str([h_list[i][1][pref] for pref in h_docs_assigned_heaps[i]]))
+                if min_doc_removed_pref>h_list[h][0][d]: #if d has a better rank than the worst ranked doc's rank
+                    #add d to assigned and add d to h_docs_assigned_heaps[h][0]
+                    print("assigned was: "+str(assigned))
+                    assigned.add(d)
+                    print("assigned is "+str(assigned))
+                    print("unassigned was: " + str(unassigned))
+                    unassigned.remove(d)
+                    print("unassigned is: " + str(unassigned))
+                    heapq.heappush(h_docs_assigned_heaps[h],h_list[h][0][d])
+
+                    print("assigned was: " + str(assigned))
+                    assigned.remove(min_doc_removed)
+                    print("assigned is " + str(assigned))
+                    print("unassigned was: " + str(unassigned))
+                    unassigned.add(min_doc_removed)
+                    print("unassigned is: " + str(unassigned))
+                else: #cannot replace anyone
+                    heapq.heappush(h_docs_assigned_heaps[h],min_doc_removed_pref)
+                    continue
+            else: #some space is still left
+                heapq.heappush(h_docs_assigned_heaps[h],h_list[h][0][d])
+                print("assigned doc "+str(d)+" to hospital "+str(h))
+                assigned.add(d)
+                print("unassigned-")
+                print(unassigned)
+                unassigned.remove(d)
+    if start_from[d]>= h: #no more hospitals left to start
+        if d in unassigned:
+            unassigned.remove(d)
+            cant_assign.add(d)
+            if d in assigned:
+                print("Wtf")
+print("assigned -")
+print(assigned)
+print("unassigned -")
+print(unassigned)
+print("can't assign -")
+print(cant_assign)
+print("h is "+str(h))
+for i in range(hd[0]):
+    print("hospital "+str(i)+" has the following doctors")
+    for pref in h_docs_assigned_heaps[i]:
+        print(h_list[i][1][pref])
