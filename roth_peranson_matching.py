675ee89a9b0f9624c8cb1fffe9dd6fb442a46d1f
Varun Ramanathan <varun28031999@gmail.com>
2018-06-30 15:15:34 +0530

    Added input1.txt which is from the NMRP Match video. Also, edited the main algorithm - the only required change is to convert the heap to a maxheap

--- roth_peranson_matching.py
+++ roth_peranson_matching.py
@@ -42,65 +42,74 @@ h_docs_assigned_heaps = [[] for i in range(h+1)]
 while(unassigned):
     d = unassigned.pop()
     unassigned.add(d)
-    print("popping "+str(d)+" right now (but also added it)")
-    for h in d_list[d][0]:
-        print("looking at hospital "+str(h))
-        if h<start_from[d]:
-            print("already looked at this hospital for this doctor")
+    print("Doctor: "+str(d))
+    for pref in range(len(d_list[d][1])):
+        h = d_list[d][1][pref]
+        print("Hospital "+str(h))
+        if pref<start_from[d]:
+            print("Already done with hospital "+str(h))
             continue #if you have already evaluated this hospital, you won't need to come here again
-        start_from[d] = h+1
+        start_from[d] = pref+1
         if d not in h_list[h][0]: #d is not acceptable for hospital h
-            print(str(d)+" is not accepted by "+str(h))
+            print("Doctor "+str(d)+" is not accepted by hospital"+str(h)+".")
             continue
         else:
+            print("Doctor " + str(d) + " IS accepted by hospital" + str(h) + ".")
             if len(h_docs_assigned_heaps[h])==h_l_u[h][0]: #all places are filled
-                print("all the places have been filled at "+str(h))
+                print("But all the places have been filled at hospital "+str(h)+".")
+                print("Doctors at hospital "+str(h)+" are : "+str(h_docs_assigned_heaps[h])+".")
                 min_doc_removed_pref = heapq.heappop(h_docs_assigned_heaps[h]) #the worst ranked doc's ranking
                 min_doc_removed = h_list[h][1][min_doc_removed_pref]
-
+                print("Lowest ranked doctor in hospital "+str(h)+" right now is "+str(min_doc_removed)+" with preference "+str(min_doc_removed_pref)+".")
                 if min_doc_removed_pref>h_list[h][0][d]: #if d has a better rank than the worst ranked doc's rank
+                    print("But the preference of present doctor "+str(d)+" is "+str(h_list[h][0][d])+", which is better.")
                     #add d to assigned and add d to h_docs_assigned_heaps[h][0]
-                    print("assigned was: "+str(assigned))
+                    # print("assigned was: "+str(assigned))
                     assigned.add(d)
-                    print("assigned is "+str(assigned))
-                    print("unassigned was: " + str(unassigned))
+                    # print("assigned is "+str(assigned))
+                    # print("unassigned was: " + str(unassigned))
                     unassigned.remove(d)
-                    print("unassigned is: " + str(unassigned))
+                    # print("unassigned is: " + str(unassigned))
                     heapq.heappush(h_docs_assigned_heaps[h],h_list[h][0][d])
 
-                    print("assigned was: " + str(assigned))
+                    # print("assigned was: " + str(assigned))
                     assigned.remove(min_doc_removed)
-                    print("assigned is " + str(assigned))
-                    print("unassigned was: " + str(unassigned))
+                    # print("assigned is " + str(assigned))
+                    # print("unassigned was: " + str(unassigned))
                     unassigned.add(min_doc_removed)
-                    print("unassigned is: " + str(unassigned))
+                    # print("unassigned is: " + str(unassigned))
+                    print("So removing "+str(min_doc_removed)+" and adding "+str(d)+" to hospital "+str(h)+".")
                     break
                 else: #cannot replace anyone
+                    print("But the preference of present doctor " + str(d) + " is " + str(h_list[h][0][
+                        d]) + ", which is NOT better.")
                     heapq.heappush(h_docs_assigned_heaps[h],min_doc_removed_pref)
                     continue
             else: #some space is still left
+                print("And, there is space!")
                 heapq.heappush(h_docs_assigned_heaps[h],h_list[h][0][d])
-                print("assigned doc "+str(d)+" to hospital "+str(h))
+                print("Assigned doctor "+str(d)+" to hospital "+str(h)+".")
                 assigned.add(d)
-                print("unassigned-")
-                print(unassigned)
+                # print("unassigned-")
+                # print(unassigned)
                 unassigned.remove(d)
-                print("unassigned-")
-                print(unassigned)
+                # print("unassigned-")
+                # print(unassigned)
                 break
-    if start_from[d]>= h: #no more hospitals left to start
+    if start_from[d]>= len(d_list[d][0]): #no more hospitals left to start
+        print("All hospitals for doctor "+str(d)+" have been checked already.")
         if d in unassigned:
             unassigned.remove(d)
             cant_assign.add(d)
             if d in assigned:
                 print("Wtf")
-print("assigned -")
-print(assigned)
-print("unassigned -")
-print(unassigned)
-print("can't assign -")
-print(cant_assign)
-print("h is "+str(h))
+# print("assigned -")
+# print(assigned)
+# print("unassigned -")
+# print(unassigned)
+# print("can't assign -")
+# print(cant_assign)
+# print("h is "+str(h))
 for i in range(1,hd[0]+1):
     print("hospital "+str(i)+" has the following doctors")
     for pref in h_docs_assigned_heaps[i]:
